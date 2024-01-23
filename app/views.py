b75953ac197853c3d68d124d19869813c69cfe34
subham31p <subhampatro3102@gmail.com>
2024-01-23 19:10:57 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,36 +1,102 @@
 from django.shortcuts import render
-from app.forms import *
-from django.http import HttpResponse
-from django.core.mail import send_mail
 
 # Create your views here.
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login
+from django.urls import reverse
+
+
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
 def registration(request):
-    ufo=UserForm()
-    pfo=ProfileForm()
-    d={'ufo':ufo,'pfo':pfo}
-    
+    uf=UserForm()
+    pf=ProfileForm()
+    d={'uf':uf,'pf':pf}
+
     if request.method=='POST' and request.FILES:
-        ufd=UserForm(request.POST)
-        pfd=ProfileForm(request.POST,request.FILES)
-        if ufd.is_valid() and pfd.is_valid():
-            MUFDO=ufd.save(commit=False)
-            pw=ufd.cleaned_data['password']
-            MUFDO.set_password(pw)
-            MUFDO.save()
-            MPFDO=pfd.save(commit=False)
-            MPFDO.username=MUFDO
-            MPFDO.save()
-            send_mail('Sample Test Message',
-                      'Hello! Your Account Registration is Successful',
-                      'subhampatro255@gmail.com',
-                      [MUFDO.email],
-                      fail_silently=False)
+        UFD=UserForm(request.POST)
+        PFD=ProfileForm(request.POST,request.FILES)
+        if UFD.is_valid() and PFD.is_valid():
+            UFO=UFD.save(commit=False)
+            password=UFD.cleaned_data['password']
+            UFO.set_password(password)
+            UFO.save()
+
+            PFO=PFD.save(commit=False)
+            PFO.profile_user=UFO
+            PFO.save()
+
+            send_mail('registration',
+            'Thanks for registartion,ur registration is Successfull',
+            'subhampatro255@gmail.com',
+            [UFO.email],
+            fail_silently=False
             
-            
-            return HttpResponse('Registration done successfully')
+            )
+            return HttpResponse('registration is successffull')
+
+
+    return render(request,'registration.html',d)
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        user=authenticate(username=username,password=password)
+
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
         else:
-            return HttpResponse('Registration Unsuccessfull')
-            
-    
-    
-    return render(request, 'registration.html',d)
+            return HttpResponse('u r not an authenticated user')
+    return render(request,'user_login.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- project32/urls.py
+++ project32/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for project30 project.
+"""project27 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,17 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import *
 from django.conf.urls.static import static
 from django.conf import settings
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home,name='home'),
     path('registration/',registration,name='registration'),
+    path('user_login/',user_login,name='user_login'),
+    
 
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
